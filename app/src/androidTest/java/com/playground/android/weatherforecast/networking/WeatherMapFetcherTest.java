94c254d7762e446e350118ac417049452b32cb8d
Anandha Rajendran <anarajendran@ebay.com>
2015-09-21 19:07:55 -0700

    Adding jUnit test for networking class

---
+++ app/src/androidTest/java/com/playground/android/weatherforecast/networking/WeatherMapFetcherTest.java
@@ -0,0 +1,94 @@
+package com.playground.android.weatherforecast.networking;
+
+import android.location.Location;
+
+import com.playground.android.weatherforecast.bean.CurrentWeatherReport;
+import com.playground.android.weatherforecast.bean.WeatherForecastReport;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.util.List;
+
+public class WeatherMapFetcherTest extends TestCase {
+
+    WeatherMapFetcher mWeatherMapFetcher;
+    Location mockLocation;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mWeatherMapFetcher = new WeatherMapFetcher();
+        mockLocation = new Location("Mock");
+        mockLocation.setLatitude(45.513904);
+        mockLocation.setLongitude(-122.6799639);
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testFetchCurrentWeatherReport() throws Exception {
+        CurrentWeatherReport currentWeatherReport = mWeatherMapFetcher.fetchCurrentWeatherReport("portland");
+        Assert.assertNotNull(currentWeatherReport);
+        Assert.assertNotNull(currentWeatherReport.getWeaterIcon());
+        Assert.assertNotNull(currentWeatherReport.getTemperature());
+        Assert.assertNotNull(currentWeatherReport.getDate());
+        Assert.assertNotNull(currentWeatherReport.getDescription());
+        Assert.assertNotNull(currentWeatherReport.getHumidity());
+        Assert.assertTrue(currentWeatherReport.getLocation().contains("Portland"));
+        Assert.assertNotNull(currentWeatherReport.getPressure());
+        Assert.assertNotNull(currentWeatherReport.getWind());
+    }
+
+    public void testFetchCurrentWeatherReport1() throws Exception {
+
+        CurrentWeatherReport currentWeatherReport = mWeatherMapFetcher.fetchCurrentWeatherReport(mockLocation);
+        Assert.assertNotNull(currentWeatherReport);
+        Assert.assertNotNull(currentWeatherReport.getWeaterIcon());
+        Assert.assertNotNull(currentWeatherReport.getTemperature());
+        Assert.assertNotNull(currentWeatherReport.getDate());
+        Assert.assertNotNull(currentWeatherReport.getDescription());
+        Assert.assertNotNull(currentWeatherReport.getHumidity());
+        Assert.assertTrue(currentWeatherReport.getLocation().contains("Portland"));
+        Assert.assertNotNull(currentWeatherReport.getPressure());
+        Assert.assertNotNull(currentWeatherReport.getWind());
+    }
+
+    public void testFetchWeatherForecastReport() throws Exception {
+        List<WeatherForecastReport> weatherForecastReport= mWeatherMapFetcher.fetchWeatherForecastReport("chennai");
+
+        Assert.assertNotNull(weatherForecastReport);
+        for (WeatherForecastReport report : weatherForecastReport){
+            Assert.assertNotNull(report.getDate());
+            Assert.assertNotNull(report.getTemperature());
+            Assert.assertNotNull(report.getWeaterIcon());
+        }
+    }
+
+    public void testFetchWeatherForecastReport1() throws Exception {
+        List<WeatherForecastReport> weatherForecastReport= mWeatherMapFetcher.fetchWeatherForecastReport(mockLocation);
+
+        Assert.assertNotNull(weatherForecastReport);
+        for (WeatherForecastReport report : weatherForecastReport){
+            Assert.assertNotNull(report.getDate());
+            Assert.assertNotNull(report.getTemperature());
+            Assert.assertNotNull(report.getWeaterIcon());
+        }
+    }
+
+    public void testParseWeatherForecast() throws Exception {
+
+    }
+
+    public void testGetJsonBody() throws Exception {
+
+    }
+
+    public void testGetUrlBytes() throws Exception {
+
+    }
+
+    public void testGetUrlString() throws Exception {
+
+    }
+}
